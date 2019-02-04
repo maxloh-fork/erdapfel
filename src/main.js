a47e506e22ce4e0d147108b30a948545c773bd7f
jlaville <j.laville@qwant.com>
2019-02-04 14:45:05 +0100

    ini prettier

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 import "@babel/polyfill";
-import I18n from './libs/i18n'
-import './libs/listen'
-import './libs/actions'
-import App from './panel/app_panel'
+import I18n from "./libs/i18n";
+import "./libs/listen";
+import "./libs/actions";
+import App from "./panel/app_panel";
 import PanelManager from "./proxies/panel_manager";
 import UrlState from "./proxies/url_state";
 
-
 (async function main() {
-  new I18n()
-  await setLang()
+  new I18n();
+  await setLang();
 
-  PanelManager.init()
-  UrlState.init()
-  new App('panels')
+  PanelManager.init();
+  UrlState.init();
+  new App("panels");
 
-  UrlState.load()
-})()
+  UrlState.load();
+})();
